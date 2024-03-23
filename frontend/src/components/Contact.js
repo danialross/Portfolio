078c0050d7be457d159acd6e17cadd52ea9b7832
danialross <92746365+danialross@users.noreply.github.com>
2024-03-23 14:09:55 -0400

    contact include linkedin
    add linkedin button and button type to array for maintainability

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,5 +1,8 @@
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
 import DisplayButton from "./DisplayButton";
+import Link from "./Link";
 
 function Contact() {
   const contacts = [
@@ -7,11 +10,20 @@ function Contact() {
       icon: faPhone,
       size: "2x",
       text: "+1 (514) 834-6110",
+      button: DisplayButton,
     },
     {
       icon: faEnvelope,
       size: "2x",
       text: "Danialrossar@gmail.com",
+      button: DisplayButton,
+    },
+    {
+      icon: faLinkedinIn,
+      size: "2x",
+      height: 20,
+      link: "https://www.linkedin.com/in/danial-ross-842579214/",
+      button: Link,
     },
   ];
 
@@ -23,11 +35,13 @@ function Contact() {
         </div>
         <div className="flex flex-col items-center p-7 gap-10 pb-10">
           {contacts.map((item, index) => (
-            <DisplayButton
+            <item.button
               key={index}
               icon={item.icon}
               size={item.size}
               text={item.text}
+              link={item.link}
+              height={item.height}
             />
           ))}
         </div>
