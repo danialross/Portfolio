6637bd8dc7f5194087fe1168bafa7db8c8c4a83e
danialross <92746365+danialross@users.noreply.github.com>
2024-03-23 18:35:14 -0400

    add copy button and alert
    user can copy the text and an alert will be shown to the user

--- frontend/src/components/DisplayButton.js
+++ frontend/src/components/DisplayButton.js
@@ -1,16 +1,27 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function DisplayButton({ icon, text, size }) {
+function DisplayButton({ icon, text, size, copy }) {
   const [isExpanded, setIsExpanded] = useState(false);
-  // const spacer =
+  const [isAlerting, setIsAlerting] = useState(false);
+
+  const copyText = () => {
+    navigator.clipboard.writeText(text);
+  };
+
+  const notifyUser = () => {
+    setIsAlerting(true);
+    setTimeout(() => {
+      setIsAlerting(false);
+    }, 2000);
+  };
 
   return (
     <button
-      className={` font-medium text-md h-20 p-[1.4rem] flex justify-start items-center justify border-darkTone border-2 rounded-xl  transition-all duration-500 ease-in-out  ${
+      className={`font-medium text-md h-20 p-[1.4rem] flex justify-start items-center justify border-darkTone border-2 rounded-xl  transition-all duration-500 ease-in-out  ${
         isExpanded
-          ? "w-80 bg-darkTone text-white"
-          : "w-20 bg-none text-darkTone"
+          ? "max-w-full bg-darkTone text-white"
+          : "max-w-20 bg-none text-darkTone"
       }`}
       onClick={() => {
         setIsExpanded(!isExpanded);
@@ -18,13 +29,36 @@ function DisplayButton({ icon, text, size }) {
     >
       <FontAwesomeIcon size={size} icon={icon} />
       <div
-        className={` p-2 relative font-racing text-xl transition-opacity text-nowrap ${
+        className={` p-2 font-racing text-xl transition-opacity text-nowrap ${
           isExpanded
             ? "delay-300 opacity-100 ease-in duration-300 visible"
             : "opacity-0 ease-in duration-200 invisible"
         }`}
       >
         {text}
+      </div>
+
+      <FontAwesomeIcon
+        className={` relative p-2 font-racing text-xl transition-opacity text-nowrap ${
+          isExpanded
+            ? "delay-300 opacity-100 ease-in duration-300 visible"
+            : "opacity-0 ease-in duration-200 invisible"
+        }`}
+        onClick={(event) => {
+          event.stopPropagation();
+          copyText();
+          notifyUser();
+        }}
+        size="1x"
+        icon={copy}
+      />
+
+      <div
+        className={`fixed flex justify-center items-center bg-white rounded-md text-darkTone transition-all duration-100 bottom-5 left-1/2 -translate-x-1/2 ${
+          isAlerting ? "w-2/3 h-10 opacity-100" : "w-0 h-0 opacity-0"
+        }`}
+      >
+        Copied
       </div>
     </button>
   );
